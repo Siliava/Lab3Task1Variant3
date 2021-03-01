fca311978a84fe40d8efeb90efbd84ba83ed74b6
Siliava <47508228+Siliava@users.noreply.github.com>
2021-03-01 12:53:41 +0300

    1

--- Lab3Task1Variant3/Program.cs
+++ Lab3Task1Variant3/Program.cs
@@ -4,7 +4,7 @@ namespace Lab3Task1Variant3
 {
     class Program
     {
-        static double Summary (double x, double e)
+        static double Summary (double x, double eps)
         {
             double sum = 0.0;
             int n = 0;
@@ -12,7 +12,7 @@ namespace Lab3Task1Variant3
             {
                 double item = 1.0/(2*n+1)*Math.Pow((x-1)/(x+1), 2*n+1);
                 n++;
-                if (Math.Abs(item) < e)
+                if (Math.Abs(item) < eps)
                     break;
                 sum += item;
                 Console.WriteLine("n={0}\titem={1:#0.000000}\tsum={2:#0.000000}", n, item, sum);
@@ -22,10 +22,12 @@ namespace Lab3Task1Variant3
            
         static void Main(string[] args)
         {
-            double x = 1.1;
-            double e = 0.000001;
-            double sum = Summary(x, e);
-            Console.WriteLine("\nСумма ряда для х={0} (с точностью e={1:#0.000000} равна: {2:#0.000000}", x, e, sum);
+            Console.WriteLine("Enter x");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter eps");
+            double eps = double.Parse(Console.ReadLine());
+            double sum = Summary(x, eps);
+            Console.WriteLine("\nСумма ряда для х={0} (с точностью e={1:#0.000000} равна: {2:#0.000000}", x, eps, sum);
             Console.ReadKey();
 
 
